b53f46c8fcbb791242717074c3757e732578c5ac
Rishabh Das <rishabh.babi@gmail.com>
2026-06-04 20:33:44 +0530

    .

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Build script simplified - no protobuf compilation needed for Kafka-only architecture
-    println!("cargo:rerun-if-changed=build.rs");
-    Ok(())
-}
